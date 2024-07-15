31804ba9a760e2bf8a70c81f1b7730b8a2d84c42
JocelynTereucan <38298509+JocelynTereucan@users.noreply.github.com>
2024-07-14 20:15:24 -0400

    Algunas consultas

---
+++ SQL_consultas.sql
@@ -0,0 +1,10 @@
+USE Covid19_Analysis2;
+
+-- Verificar datos en Dim_Region
+SELECT TOP 10 * FROM Dim_Region;
+
+-- Verificar datos en Dim_Tiempo
+SELECT TOP 10 * FROM Dim_Tiempo;
+
+-- Verificar datos en Hechos_Covid
+SELECT TOP 1500 * FROM Hechos_Covid;
